ddc9a2421c4b7fd62dac90bf4c68e3215c1d68c4
einkelflugle <cluckyferret@gmail.com>
2015-05-09 17:13:57 +1000

    Add Rocket plugin

---
+++ Blitz/Blitz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Rocket.RocketAPI;
+
+namespace Blitz
+{
+	public class Blitz : RocketPlugin
+	{
+		public static Blitz Instance;
+
+		protected override void Load()
+		{
+			Instance = this;
+		}
+	}
+}
+
