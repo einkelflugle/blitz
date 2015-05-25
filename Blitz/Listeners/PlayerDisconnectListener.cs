a459a63de9c0882b33b947a8eae518bcd3611ac2
einkelflugle <cluckyferret@gmail.com>
2015-05-25 21:43:54 +1000

    Remove player from team on disconnect
    Fixes crash.

---
+++ Blitz/Listeners/PlayerDisconnectListener.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+
+namespace Blitz
+{
+	public class PlayerDisconnectListener
+	{
+		public PlayerDisconnectListener ()
+		{
+			RocketServerEvents.OnPlayerDisconnected += new RocketServerEvents.PlayerDisconnected(this.onPlayerDisconnect);
+		}
+
+		private void onPlayerDisconnect (RocketPlayer player)
+		{
+			Team t = Team.ForPlayer (player);
+			t.RemovePlayer (PlayerData.ForPlayer (player));
+		}
+	}
+}
+
